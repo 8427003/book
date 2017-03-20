cd8f810fef841e01a5e8967008b1d89bc5686db8
lijun <lijun@lijundeMacBook-Pro.local>
2017-03-21 00:39:07 +0800

    change sh

--- deploy-to-vps.sh
+++ deploy-to-vps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf ./www-bk && git clone https://github.com/lijun401338/lijun401338.github.io.git www-bk
+rm -rf ./www-bk && git clone --depth=1 https://github.com/lijun401338/lijun401338.github.io.git www-bk
 
 rm -rf ./www && mv www-bk www
